9e7036771896a6fce126ac5238df15f50dc43153
BitesZ <pedroricardost040502@gmail.com>
2023-11-09 03:24:09 -0300

    Atividade 5 - Numero Antecessor

---
+++ Numero-antecessor/Atividade 5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//INICIO
+            Console.WriteLine("Digite um número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1--);
+            Console.WriteLine("O valor antecessor a este é: " + num1);
+            Console.ReadKey();
+        }//FIM
+    }
+}
