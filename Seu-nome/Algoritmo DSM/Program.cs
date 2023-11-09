30c92066f9df422d7ab34b3b585c8724e2dfff2e
BitesZ <pedroricardost040502@gmail.com>
2023-11-09 03:10:24 -0300

    Atividade 1 - Digitar e mostrar nome

---
+++ Seu-nome/Algoritmo DSM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_DSM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//inicio
+            string nome;
+            Console.WriteLine("Qual o seu nome? ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Seu Nome é: " + nome);
+            Console.ReadLine();
+            Console.ReadKey();
+
+
+
+
+
+        }//fim
+    }
+}
