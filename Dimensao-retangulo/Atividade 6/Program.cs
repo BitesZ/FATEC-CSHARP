a9497073cf43558f950e4c5694a3b63f6cf24de7
BitesZ <pedroricardost040502@gmail.com>
2023-11-09 03:26:20 -0300

    Atividade 6 - Calculo da Dimensao de um retangulo

---
+++ Dimensao-retangulo/Atividade 6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Inicio
+            Console.WriteLine("Qual a Altura do Retângulo");
+            double Altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Qual a Base do Retângulo");
+            double Base = Convert.ToDouble(Console.ReadLine());
+            double Dimensão = Altura / Base;
+            Console.WriteLine("A Altura é: " + Altura + " e sua Base é: " + Base + " portanto sua Dimensão é: " + Dimensão);
+            Console.ReadKey();
+        }//Fim
+    }
+}
