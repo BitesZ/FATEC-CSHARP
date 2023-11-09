139ad3471f2b34f7ea731619143e3b75209b3234
BitesZ <pedroricardost040502@gmail.com>
2023-11-09 03:42:02 -0300

    Atividade 8 - Funções Alfanumericas (Parte 3)

---
+++ Alfa-num3/Atividade 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//inicio
+            string nome, nome_e;
+            int pos;
+            Console.WriteLine("Bem vindo ao gerador de e-mails fatec");
+            Console.Write("Digite seu nome completo: ");
+            nome = Console.ReadLine();
+            pos = nome.IndexOf(" ");
+            nome_e = nome.Substring(0, pos);
+            Console.WriteLine("Seu email institucional é: " + nome_e + "@fatec.sp.gov.br");
+            Console.ReadKey();
+        }//fim
+    }
+}
