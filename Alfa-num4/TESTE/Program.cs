4d0eb283638b57bfeba8adb4e79b9a505d30b29e
BitesZ <pedroricardost040502@gmail.com>
2023-11-09 03:42:37 -0300

    Atividade 8 - Funções Alfanumericas (Parte 4)

---
+++ Alfa-num4/TESTE/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, sobrenome, nome_p;
+            int pos, cos;
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+            pos = nome.IndexOf("R");
+            nome_p = nome.Substring(0, pos);
+            Console.WriteLine(nome_p);
+            Console.ReadKey();
+        }
+    }
+}
