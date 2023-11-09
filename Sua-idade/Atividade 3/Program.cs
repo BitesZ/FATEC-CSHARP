466bb48c80b528805dbacd0b82f46a29780ac253
BitesZ <pedroricardost040502@gmail.com>
2023-11-09 03:19:24 -0300

    Atividade 3 - Verificando qual a idade do usuário

---
+++ Sua-idade/Atividade 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//INICIO
+            Console.WriteLine("Qual o ano de seu nascimento?");
+            Double ano_nasc = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Qual o ano Atual?");
+            Double ano_atual = Convert.ToDouble(Console.ReadLine());
+            Double idade = ano_atual - ano_nasc;
+            Console.WriteLine("Sua idade é: " + idade);
+            Console.ReadKey();
+        }//FIM
+    }
+}
